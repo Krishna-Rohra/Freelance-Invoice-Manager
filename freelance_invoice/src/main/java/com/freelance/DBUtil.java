884be7d497b40994736a9506caaaae41326d003a
Krishna Rohra <166389825+Krishna-Rohra@users.noreply.github.com>
2025-07-10 22:21:24 +0530

    Update DBUtil.java

--- freelance_invoice/src/main/java/com/freelance/DBUtil.java
+++ freelance_invoice/src/main/java/com/freelance/DBUtil.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBUtil {
     private static final String URL = "jdbc:postgresql://localhost:5432/freelancedb";
-    private static final String USER = "postgres"; // change if needed
-    private static final String PASSWORD = "@Kris#008"; // change this
+    private static final String USER = "postgres"; 
+    private static final String PASSWORD = "@Kris#008";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
